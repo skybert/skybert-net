dda0429d04b40e4fc82a5967e8021276d5b4c0c0
Torstein Krause Johansen <torstein@escenic.com>
2015-12-15 22:44:35 +0100

    Simplified conf

--- conf/pelicanconf.py
+++ conf/pelicanconf.py
@@ -5,11 +5,7 @@ from __future__ import unicode_literals
 AUTHOR = 'Torstein Krause Johansen'
 SITENAME = "Skybert's World"
 SITEURL = 'http://skybert.net'
-
-PATH = 'src'
-
 TIMEZONE = 'Europe/Oslo'
-
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
@@ -21,37 +17,15 @@ TRANSLATION_FEED_ATOM = None
 TAG_CLOUD_STEPS = 8
 TAG_CLOUD_MAX_ITEMS = 100
 
-# Blogroll
-LINKS = None
-
-# Social widget
-SOCIAL = (('Twitter (tech)', 'https://twitter.com/torsteinkrausew'),
-          ('Twitter (personal)', 'https://twitter.com/torsteinkrause'),
-          ('Github', 'https://github.com/skybert'),
-          ('Instagram', 'http://instagram.com/torsteinkrause'),
-)
-
 DEFAULT_PAGINATION = 10
 
-# Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
-
 TWITTER_USERNAME = "torsteinkrause"
-
 THEME = "themes/skybert"
 
-# nmnlist
-# waterspill
-# cebong
-
-# CATEGORY_URL = "c/{slug}.html"
-
-
+PATH = 'src'
 ARTICLE_URL = "{category}/{slug}"
 ARTICLE_SAVE_AS = "{category}/{slug}/index.html"
-
 PAGE_URL = "{category}/{slug}"
 PAGE_SAVE_AS = "{category}/{slug}/index.html"
-
 STATIC_PATHS = ['graphics']
-
