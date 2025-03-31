391dab1ca5b4479cb1faa8d799b99962b48ea0ef
Torstein Krause Johansen <torstein@skybert.net>
2025-03-31 10:27:22 +0200

    Convert PNGs and JPGs to webp
    - WebP has a good balance of quality and compression and works in
    all (fingers crossed) browsers that people use.
    
    Conflicts:
    	src/graphics/2025/convert2webp.sh

--- src/graphics/2025/convert2webp.sh
+++ src/graphics/2025/convert2webp.sh
@@ -1,1 +1,17 @@
-for el in *.png *.jpg ; do cwebp -q 80 $el -o $(basename $el).webp ; done
+#! /usr/bin/env bash
+
+## author: torstein
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+main() {
+  for el in *.png *.jpg ; do
+    bn=$(basename "${el}" .png)
+    bn=$(basename "${bn}" .jpg)
+    cwebp -q 80 "${el}" -o "${bn}".webp
+  done
+}
+
+main "$@"
